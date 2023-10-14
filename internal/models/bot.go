a38914c0575588eedb4899f03038397dd7db6dc9
Freddy Ouellette <freddy.ouellette@gmail.com>
2023-10-14 15:29:37 +0000

    add gorm and json field names

--- internal/models/bot.go
+++ internal/models/bot.go
@@ -1,10 +1,13 @@
 package models
 
+import "gorm.io/gorm"
+
 type Bot struct {
-	Id          string
-	Name        string
-	Description string
-	Model       string
-	Personality string
-	UserHistory string
+	gorm.Model
+	Uuid        string `json:"uuid"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	AiModel     string `json:"model"`
+	Personality string `json:"personality"`
+	UserHistory string `json:"user_history"`
 }
--- internal/services/bots/bots.go
+++ internal/services/bots/bots.go
@@ -14,10 +14,10 @@ func NewBotService() *BotService {
 
 func (s *BotService) GetBots() ([]models.Bot, error) {
 	return []models.Bot{{
-		Id:          "1",
+		Uuid:        "9f976d75-8aec-4d92-a9e5-c7e810f7051f",
 		Name:        "Bot 1",
 		Description: "Bot 1 description",
-		Model:       "Bot 1 model",
+		AiModel:     "Bot 1 model",
 		Personality: "Bot 1 personality",
 		UserHistory: "Bot 1 user history",
 	}}, nil
