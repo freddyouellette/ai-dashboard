c957f0237679bdbb0d00a5e36a3f82951b53af0e
Freddy Ouellette <freddy.ouellette@gmail.com>
2023-10-14 15:48:42 +0000

    change to docker compose devcontainer

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,15 +2,19 @@ package main
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/freddyouellette/ai-dashboard/internal/api/error_handler"
 	"github.com/freddyouellette/ai-dashboard/internal/api/request_controller"
 	"github.com/freddyouellette/ai-dashboard/internal/api/response_handler"
 	"github.com/freddyouellette/ai-dashboard/internal/api/router"
 	"github.com/freddyouellette/ai-dashboard/internal/services/bots"
+	_ "github.com/joho/godotenv/autoload"
 )
 
 func main() {
+	API_PORT := os.Getenv("API_PORT")
+
 	errorHandler := error_handler.NewErrorHandler()
 	responseHandler := response_handler.NewResponseHandler(errorHandler)
 	botService := bots.NewBotService()
@@ -21,5 +25,5 @@ func main() {
 	router := router.NewRouter(requestController)
 
 	http.Handle("/", router)
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":"+API_PORT, nil)
 }
