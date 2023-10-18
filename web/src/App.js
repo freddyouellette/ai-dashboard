f432049d1ddeb30d59c1d2caee0a31d79f995bd8
Freddy Ouellette <freddy.ouellette@gmail.com>
2023-10-18 05:58:14 +0000

    move BotForm

--- web/src/App.js
+++ web/src/App.js
@@ -4,6 +4,7 @@ import { faEdit, faPlus } from '@fortawesome/free-solid-svg-icons'
 import './App.css';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import BotForm from './forms/BotForm';
 
 const PAGE_CONTENT_CREATE_BOT = "create-bot";
 
@@ -49,7 +50,7 @@ function App() {
 	let content;
 	switch(pageContent) {
 		case PAGE_CONTENT_CREATE_BOT:
-			content = <CreateBotForm upsertBotFunc={upsertBot} botToUpdate={botToUpdate} />;
+			content = <BotForm upsertBotFunc={upsertBot} botToUpdate={botToUpdate} />;
 		break;
 		default:
 			content = <div className="text-center text-italics">Select a bot</div>;
@@ -91,66 +92,6 @@ function App() {
 				</Container>
 			</Col>
 		</Row>
-	);
-}
-
-function CreateBotForm({upsertBotFunc, botToUpdate}) {
-	const [botFormData, setBotFormData] = useState(null);
-	
-	// update the state if the botToUpdate prop changes
-	useEffect(() => {
-		setBotFormData(botToUpdate);
-	}, [botToUpdate])
-	
-	let handleChange = (event) => {
-		setBotFormData({
-			...botFormData,
-			[event.target.name]: event.target.value
-		});
-	}
-	
-	let handleSubmit = async (event) => {
-		event.preventDefault();
-		
-		axios[botFormData.ID ? "put" : "post"]("http://localhost:8080/bots", botFormData)
-		.then(response => {
-			console.log(response);
-			upsertBotFunc(response.data);
-		}).catch(error => {
-			console.error(error);
-		})
-	}
-	
-	return (
-		<div>
-			<h1>{botFormData?.ID ? 'Update Bot' : 'Create New Bot'}</h1>
-			<form onSubmit={handleSubmit}>
-				<div className="mb-3">
-					<label htmlFor="name" className="form-label">Bot Name*</label>
-					<input onChange={handleChange} type="text" className="form-control" id="create-bot-form-name" name="name" placeholder="Enter bot name" required value={botFormData?.name || ''} />
-				</div>
-				<div className="mb-3">
-					<label htmlFor="description" className="form-label">Description</label>
-					<input onChange={handleChange} type="text" className="form-control" id="create-bot-form-description" name="description" placeholder="Enter bot description" value={botFormData?.description || ''} />
-				</div>
-				<div className="mb-3">
-					<label htmlFor="model" className="form-label">Model*</label>
-					<select onChange={handleChange} id="create-bot-form-model" name="model" className="form-control" value={botFormData?.model ||  ''}>
-						<option value="gpt-4">GPT-4</option>
-					</select>
-				</div>
-				<div className="mb-3">
-					<label htmlFor="personality" className="form-label">Personality</label>
-					<textarea onChange={handleChange} className="form-control" id="create-bot-form-personality" name="personality" rows="3" placeholder="Enter bot personality" value={botFormData?.personality ||  ''} ></textarea>
-				</div>
-				<div className="mb-3">
-					<label htmlFor="user_history" className="form-label">User History</label>
-					<textarea onChange={handleChange} className="form-control" id="create-bot-form-user_history" name="user_history" rows="3" placeholder="Enter user history" value={botFormData?.user_history ||  ''} ></textarea>
-				</div>
-				<Button className="btn-secondary me-3">Cancel</Button>
-				<button type="submit" className="btn btn-primary">{botFormData?.ID ? 'Update' : 'Create'} Bot</button>
-			</form>
-		</div>
 	);
 }
 
