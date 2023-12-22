2538faa587ce7fe9fbc849f6d16cc00afef3d04c
Freddy Ouellette <freddy.ouellette@gmail.com>
2023-12-22 22:58:38 +0000

    remove service worker because I think it's causing caching issues

--- web/src/index.js
+++ web/src/index.js
@@ -7,7 +7,6 @@ import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap';
 import { Provider } from 'react-redux';
 import store from './store/store';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -21,7 +20,7 @@ root.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.register();
+// serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
