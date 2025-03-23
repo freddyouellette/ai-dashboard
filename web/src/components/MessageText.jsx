54bf9c3d5181c7fda152a17558e22bf33bf35389
Freddy Ouellette <freddy.ouellette@gmail.com>
2025-03-23 09:03:54 +0000

    revert messagetext

--- web/src/components/MessageText.jsx
+++ web/src/components/MessageText.jsx
@@ -1,8 +1,7 @@
-// import Markdown from "react-markdown";
+import Markdown from "react-markdown";
 import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
 import "katex/dist/katex.min.css";
-import {marked} from "marked";
 
 export default function MessageText({ children }) {
 	// ChatGPT returns \( and \), and sometimes \[ and \] instead of $ for LaTeX format
@@ -10,9 +9,10 @@ export default function MessageText({ children }) {
 	children = children.replace(/\\+[()[\]](?=(?:[^`]*`[^`]*`)*[^`]*$)/g, '$');
 	// Replace \[ and \] with $$ for block LaTeX, but ignore backticks
 	children = children.replace(/\\+[[\]](?=(?:[^`]*`[^`]*`)*[^`]*$)/g, '$$');
-	children = children.replace(/<think>/g, "<details class=\"text-muted\"><summary><i>Thinking...</i></summary><i>");
-	children = children.replace(/<\/think>/g, "</i></details>");
-
-	// let hasReplacedFirstThink = false;
-	return <div dangerouslySetInnerHTML={{__html: marked.parse(children)}}/>
+	
+	return (
+		<Markdown remarkPlugins={[remarkMath]} rehypePlugins={[rehypeKatex]} className="text-break">
+			{children}
+		</Markdown>
+	)
 }
